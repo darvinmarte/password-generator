983692a2a5ca51e3d8f46e799d5d1090d533997c
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-04-14 21:32:55 -0400

    added math.random, getRandom function and var arr

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //WHEN I click the button to generate a password
 //THEN I am presented with a series of prompts for password criteria
 //WHEN prompted for password criteria
@@ -14,42 +13,112 @@
 //WHEN the password is generated
 //THEN the password is either displayed in an alert or written to the page
 
-
-
-
-
-
 // Assignment code here
 // created character banks for lowercaseChars, uppercaseChars, numbersChars, symbolsChar
-var lowercaseChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
-var uppercaseChars = ["A","B","C","D","E","F","G","H","I","J", "K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
-var numbersChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowercaseChars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercaseChars = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numbersChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbolsChar = ["!", "@", "#", "$", "%", "&", "*", "(", ")"];
 
-function generatePasswordOptions(){
-  var length = prompt("Please enter length between 8 and 128 characters.")
-  console.log(length)
-  var lowerCase = confirm("Do you want to include lowercase characters?")
-  console.log(lowerCase)
-  var upperCase = confirm("Do you want to include uppercase characters?")
-  console.log(upperCase)
-  var numeric = confirm("Would you like to include numbers?")
-  console.log(numeric)
-  var specialCharacters = confirm("Would you like to use special characters?")
-  console.log(specialCharacters)
-  if (lowerCase === false && 
+function generatePasswordOptions() {
+  var length = prompt("Please enter length between 8 and 128 characters.");
+  console.log(length);
+  // add alert that says you need 8 to 128 characters
+  var lowerCase = confirm("Do you want to include lowercase characters?");
+  console.log(lowerCase);
+  var upperCase = confirm("Do you want to include uppercase characters?");
+  console.log(upperCase);
+  var numeric = confirm("Would you like to include numbers?");
+  console.log(numeric);
+  var specialCharacters = confirm("Would you like to use special characters?");
+  console.log(specialCharacters);
+  if (
+    lowerCase === false &&
     upperCase === false &&
     numeric === false &&
-    specialCharacters === false) {
-      prompt("User should select atleast one character type!")
-      return null;
+    specialCharacters === false
+  ) {
+    alert("User should select atleast one character type!");
+    return null;
   }
+  var passwordOptions = {
+    length1: length,
+    lowerCase1: lowerCase,
+    upperCase1: upperCase,
+    numeric1: numeric,
+    specialCharacters1: specialCharacters
+  }
+  return passwordOptions;
 }
+
+function getRandom(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[randomIndex];
+  return randomElement;
+}
+
+
 
 function generatePassword() {
-generatePasswordOptions()
+ var options = generatePasswordOptions();
+ var result = [];
+ var randomCharacters = [];
+ var atlCharacters = [];
 }
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -60,7 +129,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
